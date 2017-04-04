f4c31667932c8f4ccbf5f84b75a92c40e3de72cc
Tom Higgins <tomhiggins@gmail.com>
2017-04-04 12:12:48 -0600

    Update wikipediaradio.sh

--- wikipediaradio.sh
+++ wikipediaradio.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-# 
-#
+# Wikipedia Radio
+# CC0 by Tom Higgins in service to Friend COmputer 
+#    
 # Requires IA Library https://internetarchive.readthedocs.io/en/latest/installation.html
 #          Pico2Wave  
 #          Lame
@@ -10,6 +11,7 @@
 # Change iaarchive to the Identifier for your particular IA project
 iaarchive="WikipediaRadio"
 count=0
+# Change Files to the top directory of text files 
 FILES="text/AB/wiki_*"
 for f in $FILES
 do
@@ -18,15 +20,20 @@ do
         do
           if [[ "$line" == "<doc"* ]]
           then
+          # If this is the top of a new article set title to the articles name and set up the line count
             title=$(echo "$line"|cut -d "\"" -f 6|sed 's/ //g')
             mkdir "$title"
             count=0
            elif [[ "$line" == "</doc"* ]]
+           # If this is the end of the article push the finalized mp3 to IA and clean up 
              then
              ia upload "$iaarchive" "$mp3keep" --metadata="mediatype:audio"
              mv "$mp3keep" mp3/
              rmdir "$title/"
              else
+            # If this is a line of text in the article read it and convert it to a temp mp3
+            # add the temp mp3 to the master mp3 for this article
+            # increase the line count
                 count=$((count+1))
                 wavefile="$title/"$"$title"$"."$"$count"$".wav"
                 mp3file="$title/"$"$title"$"."$"$count"$".mp3"
